48426aec8b371d474b69ef11dd9b87389c7bf3f0
Rodolpho Reis <rodolphoreis@live.com>
2024-09-04 17:36:45 +0100

    chore: add imports for Container, GameProps, Link, and BsArrowRightSquare"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/games";
 import Image from "next/image";
+import Link from "next/link";
+
+import { BsArrowRightSquare } from "react-icons/bs";
+
 
 export default function Home() {
   return (
