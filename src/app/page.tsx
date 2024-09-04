c04335ad17792fb38d333b60d41c3c02b1a9e0be
Rodolpho Reis <rodolphoreis@live.com>
2024-09-04 17:39:29 +0100

    feat: create main layout for displaying daily game with dynamic image and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,34 @@ export async function GetDalygame() {
   }
 }
 
+export default async function Home() {
   const dalyGame: GameProps = await GetDalygame();
   return (
-    <>
-      <h1>Dev Games</h1>
-    </>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center my-4 font-bold md:text-xl md:my-10">
+          Separamos um jogo exclusivo para você
+        </h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width:768px) 100vw, (max-height:1200px) 33vw"
+              />
+            </div>
+          </section>
+        </Link>
+      </Container>
+    </main>
   );
 }
