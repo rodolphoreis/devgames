ea74947489691b8d3718d79483b1ab021891be97
Rodolpho Reis <rodolphoreis@live.com>
2024-09-06 22:33:41 +0100

    chore: create new interface CardGamesProps

--- src/utils/types/games.ts
+++ src/utils/types/games.ts
@@ -7,3 +7,13 @@ export interface GameProps {
   categories: string[];
   release: string;
 }
+
+export interface CardGamesProps {
+  id: number;
+  title: string;
+  description: string;
+  image_url: string;
+  platforms: string[];
+  categories: string[];
+  release: string;
+}
