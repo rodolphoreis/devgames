81f37d7efa57229b7a6cc9d4059f395bdef20469
Rodolpho Reis <rodolphoreis@live.com>
2024-09-06 20:25:39 +0100

    style: add style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,17 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Retornar a Home</Link>
+    <div
+      className="flex flex-col w-full justify-center items-center  gap-4"
+      style={{ height: "calc(100vh - 100px)" }}
+    >
+      <h2 className="text-slate-700 font-bold text-xl">
+        Opsss..Página não encontrada =/
+      </h2>
+
+      <Link href="/" className="rounded-lg bg-gray-400 px-8 py-2 text-white">
+        Retornar a Home
+      </Link>
     </div>
   );
 }
